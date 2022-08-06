111b3863b45dfa5924a6a29364f11e3fa73602ef
Akira <54892411+docxed@users.noreply.github.com>
2022-08-07 04:15:31 +0700

    initial

---
+++ pages/index.vue
@@ -0,0 +1,217 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col sm="12" md="7" lg="7">
+      <div class="text-h4 mb-5">⏰ จับเวลาเล่นเกม</div>
+      <div class="box text-center mb-5" style="background-color: #ffffff">
+        <div class="black--text" style="font-size: 5rem">{{ timeConvert(millisecond) }}</div>
+        <div v-if="stage === null">
+          <v-btn color="dark" x-large @click="startTimer()">เริ่ม</v-btn>
+        </div>
+        <div v-else-if="stage === 'start'">
+          <v-btn color="error" x-large @click="stopTimer()">หยุด</v-btn>
+        </div>
+        <div v-else-if="stage === 'stop'">
+          <v-btn color="info" x-large @click="startTimer()">ดำเนินการต่อ</v-btn>
+        </div>
+      </div>
+      <div class="mb-2">
+        <v-select
+          v-model="player"
+          prepend-icon="mdi-account"
+          :items="['องศา', 'น้องอัญ']"
+          filled
+          label="ผู้เล่น"
+        ></v-select>
+      </div>
+      <div class="mb-2">
+        <v-row>
+          <v-col sm="3">
+            <v-text-field
+              v-model="setHour"
+              type="number"
+              min="1"
+              label="จำนวนชั่วโมง"
+              filled
+              dense
+            ></v-text-field>
+          </v-col>
+          <v-col class="">
+            <v-btn class="mb-1" color="primary" @click="increaseTimer({ type: 'hour' })"
+              >บวกชั่วโมง</v-btn
+            >
+            <v-btn class="mb-1" color="error" @click="decreaseTimer({ type: 'hour' })"
+              >ลบชั่วโมง</v-btn
+            >
+          </v-col>
+        </v-row>
+      </div>
+      <div class="mb-2">
+        <v-row>
+          <v-col sm="3">
+            <v-text-field
+              v-model="setMinute"
+              type="number"
+              min="1"
+              label="จำนวนนาที"
+              filled
+              dense
+            ></v-text-field>
+          </v-col>
+          <v-col class="">
+            <v-btn class="mb-1" color="primary" @click="increaseTimer({ type: 'minute' })"
+              >บวกนาที</v-btn
+            >
+            <v-btn class="mb-1" color="error" @click="decreaseTimer({ type: 'minute' })"
+              >ลบนาที</v-btn
+            >
+          </v-col>
+        </v-row>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import moment from "moment"
+import axios from "axios"
+import { throws } from "assert"
+const url = process.env.NUXT_ENV_ENDPOINT
+moment.locale("th")
+export default {
+  name: "IndexPage",
+  head() {
+    return {
+      title: `${this.timeConvert(this.millisecond)}`,
+      meta: [
+        {
+          name: "description",
+          content: "จับเวลาเล่นเกม",
+        },
+      ],
+    }
+  },
+  data: () => ({
+    setHour: 3,
+    setMinute: 5,
+    millisecond: 3 * 60 * 60 * 1000,
+    stage: null,
+    player: "องศา",
+  }),
+  methods: {
+    timeConvert(millisecond) {
+      // milliseconds to hours, minutes and seconds
+      return moment.utc(millisecond).format("HH:mm:ss")
+    },
+    startTimer() {
+      this.stage = "start"
+      this.timer = setInterval(() => {
+        this.millisecond -= 1000
+      }, 1000)
+    },
+    stopTimer() {
+      this.stage = "stop"
+      clearInterval(this.timer)
+    },
+    timeUp() {
+      this.stage = null
+      this.millisecond = 10 * 1000
+      clearInterval(this.timer)
+    },
+    increaseTimer(value) {
+      const { type } = value
+      let word
+      if (type === "hour") {
+        this.millisecond += this.setHour * 60 * 60 * 1000
+        word = "ชั่วโมง"
+      } else if (type === "minute") {
+        this.millisecond += this.setMinute * 60 * 1000
+        word = "นาที"
+      }
+
+      const prefix = `${moment().format("LT")} น. |`
+      let message = `${prefix} ➕ ${this.player} ได้ทำการบวก${word}เล่นเกม ${
+        type === "hour" ? this.setHour : this.setMinute
+      } ${word} เหลือเวลาเล่น ${moment.utc(this.millisecond).format("H:mm")} ชั่วโมง`
+      axios.post(`${url}/notify`, {
+        message: message,
+      })
+    },
+    decreaseTimer(value) {
+      const { type } = value
+      let word
+      if (type === "hour") {
+        this.millisecond -= this.setHour * 60 * 60 * 1000
+        word = "ชั่วโมง"
+      } else if (type === "minute") {
+        this.millisecond -= this.setMinute * 60 * 1000
+        word = "นาที"
+      }
+      const prefix = `${moment().format("LT")} น. |`
+      let message = `${prefix} ➖ ${this.player} ได้ทำการลบ${word}เล่นเกม ${
+        type === "hour" ? this.setHour : this.setMinute
+      } ${word} เหลือเวลาเล่น ${moment.utc(this.millisecond).format("H:mm")} ชั่วโมง`
+      axios.post(`${url}/notify`, {
+        message: message,
+      })
+    },
+    notifyTimer(value) {
+      const { type, time } = value
+      const prefix = `${moment().format("LT")} น. |`
+      let message
+
+      if (type === "start timer") {
+        let timeStart = new Date()
+        let timeOut = timeStart.setSeconds(timeStart.getSeconds() + time / 1000)
+        message = `${prefix} 🤣 ${this.player} เริ่มจับเวลาเล่นเกม ${moment
+          .utc(time)
+          .format("H")} ชั่วโมง และจะหมดเวลาเล่นใน ${moment(timeOut).format("LT")} น.`
+      } else if (type === "pause timer") {
+        message = `${prefix} ⏸️ ${this.player} หยุดจับเวลา และเหลือเวลาเล่นอีก ${moment
+          .utc(time)
+          .format("H:mm")} ชั่วโมง`
+      } else if (type === "resume timer") {
+        message = `${prefix} ▶️ ${this.player} จับเวลาต่อ`
+      } else if (type === "time up") {
+        message = `${prefix} 🥲 ${this.player} หมดเวลาเล่นเกมแล้ว`
+      }
+
+      axios.post(`${url}/notify`, {
+        message: message,
+      })
+    },
+  },
+  watch: {
+    millisecond(value) {
+      if (value <= 0) this.timeUp()
+    },
+    stage(newValue, oldValue) {
+      if (newValue === "start" && oldValue === null) {
+        // start timer
+        this.notifyTimer({ type: "start timer", time: this.millisecond })
+      } else if (newValue === null) {
+        // time up
+        this.notifyTimer({ type: "time up", time: this.millisecond })
+      } else if (newValue === "start") {
+        // resume timer
+        this.notifyTimer({ type: "resume timer", time: this.millisecond })
+      } else if (newValue === "stop") {
+        // pause timer
+        this.notifyTimer({ type: "pause timer", time: this.millisecond })
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+body {
+  font-family: "Kanit", sans-serif;
+}
+.box {
+  background-color: #ffffff;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.14);
+  padding: 1rem;
+  opacity: 1;
+}
+</style>
